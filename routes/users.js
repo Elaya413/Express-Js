68e289ea91c10f4a5f0e4f983fcbb8e90ab573be
Elaya413 <elayavathiselvam@gmail.com>
2023-09-09 21:15:23 +0530

    express js

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const UsersController= require ( '../controller/users' )
+const router = express.Router()
+
+router.get('/',UsersController.getUsers)
+
+router.get('/:id',UsersController.getUserById)
+
+router.post('/',UsersController.createUser)
+
+router.put('/:id',UsersController.editUserById)
+
+router.delete('/:id',UsersController.deleteUserById)
+
+
+
+module.exports= router
